f04c790621e6f6f12e5ea3cc1aac8074c7c5fba1
Rok <rokushimizu1022ghibli@gmail.com>
2021-02-17 17:12:46 +0900

    Add project files.

---
+++ src/Mod4LJT/GrabberFix.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Mod4LJT
+{
+    class GrabberFix : MonoBehaviour
+    {
+        Joint joint;
+        bool firstFrame;
+
+        public void Awake()
+        {
+            Mod.Log("Awaken");
+        }
+
+        public void Start()
+        {
+            this.joint = this.GetComponent<Joint>();
+            joint.breakForce = 35000f;
+            joint.breakTorque = 35000f;
+            Mod.Log("Started");
+            this.firstFrame = true;
+        }
+
+        public void Update()
+        {
+            if (this.firstFrame)
+            {
+                this.firstFrame = false;
+                Mod.Log("Grabber's BreakForce is " + this.joint.breakForce.ToString());
+            }
+        }
+    }
+}
---
+++ src/Mod4LJT/Mod.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Modding;
+
+namespace Mod4LJT
+{
+	public class Mod : ModEntryPoint
+	{
+		public override void OnLoad()
+		{
+			GameObject mod = new GameObject("Mod4LJT");
+			UnityEngine.Object.DontDestroyOnLoad(mod);
+			this.AddBlockScript();
+		}
+
+
+        public void AddBlockScript()
+        {
+			BlockBehaviour BB;
+			PrefabMaster.GetBlock(BlockType.Grabber, out BB);
+			BB.gameObject.AddComponent<GrabberFix>();
+        }
+
+		public static void Log(string message) => Debug.Log("4LJT Log: " + message);
+		public static void Warning(string msg) => Debug.LogWarning("4LJT Warning: " + msg);
+		public static void Error(string msg) => Debug.LogError("4LJT Error: " + msg);
+	}
+}
