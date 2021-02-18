88bd8f9d0c185a72212efff9f3eddf54a9749170
Rok <rokushimizu1022ghibli@gmail.com>
2021-02-18 16:15:43 +0900

    trivia

--- src/Mod4LJT/GrabberFix.cs
+++ src/Mod4LJT/GrabberFix.cs
@@ -5,29 +5,12 @@ namespace Mod4LJT
     class GrabberFix : MonoBehaviour
     {
         Joint joint;
-        bool firstFrame;
-
-        public void Awake()
-        {
-            Mod.Log("Awaken");
-        }
 
         public void Start()
         {
             this.joint = this.GetComponent<Joint>();
             joint.breakForce = 35000f;
             joint.breakTorque = 35000f;
-            Mod.Log("Started");
-            this.firstFrame = true;
-        }
-
-        public void Update()
-        {
-            if (this.firstFrame)
-            {
-                this.firstFrame = false;
-                Mod.Log("Grabber's BreakForce is " + this.joint.breakForce.ToString());
-            }
         }
     }
 }
