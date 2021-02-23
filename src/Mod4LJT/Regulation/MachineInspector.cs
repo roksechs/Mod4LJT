2ba844f178153f86c2b4372eedd503c5d277a340
Rok <rokushimizu1022ghibli@gmail.com>
2021-02-24 00:47:41 +0900

    misc

--- src/Mod4LJT/Regulation/MachineInspector.cs
+++ src/Mod4LJT/Regulation/MachineInspector.cs
@@ -57,10 +57,6 @@ namespace Mod4LJT.Regulation
         public void SetTankType(TankType tankType)
         {
             this.machine = Machine.Active();
-            if(machine.PlayerID != null)
-            {
-                Mod.Log(machine.PlayerID.ToString());
-            }
             this._tankType = tankType;
             //this._tankTypeInt = (int)tankType;
             this.regulations.TryGetValue((int)_tankType, out this.regulation);
